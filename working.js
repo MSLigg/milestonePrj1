1ab963f272ba7dec8e52959b386d533c6aa31db2
Mylo L <100321671+MSLigg@users.noreply.github.com>
2022-06-22 21:18:08 -0400

    somewhat working, updating logic

--- working.js
+++ working.js
@@ -5,12 +5,56 @@
     }
 // scramble tiles
     tileArray.sort(() => 0.5 - Math.random());
-}   */
+}   
 
-  var tiles = document.querySelectorAll('.playingField');
+var tiles = document.querySelectorAll('.playingField');
 
 function flipOver() {
   this.classList.toggle('flip');
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+*/
+const tiles = document.querySelectorAll('.tile');
+
+let hasFlipped = false;
+let firstTile, secondTile;
+
+function flipOver() {
+    this.classList.add('flip');
+
+if (!hasFlipped) {
+    hasFlipped = true;
+    firstTile = this;
+    return;
+ }
+   secondTile = this;
+   hasFlipped = false;
+
+   checkMatch();
+ }
+//this code will check for a match. If they match, the function will run to disable those tiles and remove the event listener
+ function checkMatch() {
+   if (firstTile === secondTile) {
+     disableTiles();
+     return;
+   }
+
+   unflipTiles();
+ }
+
+ function disableTiles() {
+   firstTile.removeEventListener('click', flipOver);
+   secondTile.removeEventListener('click', flipOver);
+ }
+
+ function unflipTiles() {
+   setTimeout(() => {
+     firstTile.classList.remove('flip');
+     secondTile.classList.remove('flip');
+   }, 1500);
+ }
+
+//this adds an event listener that clicks and toggles the flip over function on each tile
+tiles.forEach(tile => tile.addEventListener('click', flipOver));
